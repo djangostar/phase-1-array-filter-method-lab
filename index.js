4b8e20a3a37c6a412b62b43dae4b0a9474e13b8d
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-20 19:16:38 -0800

    commit tree 679b007b39b2fc707e6842509e22be374d4e4d34

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here
+//function findMatching(drivers, string) {
+//    return drivers.filter(function(driver) {
+//        if (driver.toLowerCase() === string.toLowerCase()) {
+//            return drivers
+//        }
+//    })
+//}
+
+//Refractor into an arrow function within the function declaration.
+
+function findMatching(drivers, string) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === string.toLowerCase()
+    );
+}
+
+//function fuzzyMatch(drivers, string) {
+//    return drivers.filter(function(driver) {
+//        if (driver[0] === string[0]) {
+//            return drivers
+//        }
+//    })
+//}
+
+//Refractor into an arrow function within the function declaration.
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+        (driver) => driver[0] === string[0]  
+    );
+}
+
+//function matchName(drivers, string) {
+//    return drivers.filter(function(driver){
+//        if (driver.name === string) {
+//            return drivers
+//        }
+//    })
+//}
+
+function matchName(drivers, string) {
+    return drivers.filter(
+        (driver) => driver.name === string
+    );
+}
